951bd15b6901756c52bc8f4586a9287f53734d9f
moulayprogrammer <moulay0311@gmail.com>
2022-07-19 15:59:32 +0200

    UPDATE

--- src/Controllers/InvoiceController/AddSaleController.java
+++ src/Controllers/InvoiceController/AddSaleController.java
@@ -1,11 +1,13 @@
 package Controllers.InvoiceController;
 
 import BddPackage.ConnectBD;
-import Models.ComponentProduction;
 import Models.Product;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.NodeOrientation;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -13,12 +15,13 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class AddSaleController implements Initializable {
 
     @FXML
-    TextField tfPriceProduction,tfPriceSale,tfQte,tfPriceGlobal,tfNetProfit;
+    TextField tfQte,tfPriceProductionUnit,tfPriceProduction,tfPriceUnit,tfPrice,tfNetProfit;
     @FXML
     private Button btnSale;
 
@@ -26,92 +29,122 @@ public class AddSaleController implements Initializable {
     private Connection conn;
 
     private Product selectedProduct;
+    private double price;
+    private double cost;
+    private int qte;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         conn = connectBD.connect();
+
+        tfQte.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.isEmpty()) tfQte.setText("0");
+            else Count();
+        });
+
+        tfPriceUnit.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.isEmpty()) {
+                tfPriceUnit.setText(String.valueOf(price / qte));
+                tfPrice.setText(String.format(Locale.FRANCE, "%,.2f", price ));
+                tfNetProfit.setText(String.format(Locale.FRANCE, "%,.2f", (price - cost) ));
+            }
+            else CountProfit();
+        });
     }
 
     public void Init(Product product){
 
         this.selectedProduct = product;
         tfQte.setText(String.valueOf(selectedProduct.getQte()));
-        ActionCount();
+        Count();
     }
 
-    @FXML
-    private void ActionCount(){
+    private void Count(){
         String stQte = tfQte.getText().trim();
-        if (!stQte.isEmpty()){
-            int qte = Integer.parseInt(stQte);
-            if (checkQte(qte)) {
-//                double cost = getCost();
-                double price = getPrice(qte);
+        if (!stQte.isEmpty() && !stQte.equals("0")){
+            qte = Integer.parseInt(stQte);
+            if (qte <= this.selectedProduct.getQte() ) {
+                try {
+                    if (conn.isClosed()) conn = connectBD.connect();
+                    price = 0.0;
+                    cost = 0.0;
 
+                    try {
+                        String query = "SELECT كمية_مخزنة - كمية_مستهلكة AS الكمية, سعر_البيع, التكلفة, الكمية_المنتجة FROM تخزين_منتج, الانتاج" +
+                                " WHERE  تخزين_منتج.معرف_المنتج = ? AND  كمية_مخزنة - كمية_مستهلكة > 0 AND الانتاج.المعرف = تخزين_منتج.معرف_الانتاج ORDER BY (تاريخ_التخزين) ASC;";
+                        PreparedStatement preparedStmt = conn.prepareStatement(query);
+                        preparedStmt.setInt(1,this.selectedProduct.getId());
+                        ResultSet resultSet = preparedStmt.executeQuery();
+
+                        while (resultSet.next()){
+                            int qtePro = resultSet.getInt("الكمية");
+                            double pricePro = resultSet.getDouble("سعر_البيع");
+                            double costPro = resultSet.getDouble("التكلفة") / resultSet.getInt("الكمية_المنتجة");
+
+                            if (qtePro >= qte){
+                                price += (pricePro * qte);
+                                cost += (costPro * qte );
+                                break;
+                            }else {
+                                price += (pricePro * qtePro);
+                                cost += (costPro * qtePro);
+                                qte -= qtePro;
+                            }
+                        }
+
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                    tfPriceProductionUnit.setText(String.format(Locale.FRANCE, "%,.2f",cost/qte ));
+                    tfPriceProduction.setText(String.format(Locale.FRANCE, "%,.2f", cost ));
+                    tfPriceUnit.setText(String.valueOf(price/qte));
+                    tfPrice.setText(String.format(Locale.FRANCE, "%,.2f", price ));
+                    tfNetProfit.setText(String.format(Locale.FRANCE, "%,.2f", (price - cost) ));
+
+                    conn.close();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+
+            }else {
+                Alert alertWarning = new Alert(Alert.AlertType.WARNING);
+                alertWarning.setHeaderText("الكمية ");
+                alertWarning.setContentText("الكمية غير متوفرة");
+                alertWarning.getDialogPane().setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+                Button okButton = (Button) alertWarning.getDialogPane().lookupButton(ButtonType.OK);
+                okButton.setText("موافق");
+                alertWarning.showAndWait();
+
+                tfQte.setText(String.valueOf(this.selectedProduct.getQte()));
             }
 
+        }else {
+            tfPriceProductionUnit.setText("0");
+            tfPriceProduction.setText("0");
+            tfPriceUnit.setText("0");
+            tfPrice.setText("0");
+            tfNetProfit.setText("0");
         }
     }
 
-    private boolean checkQte(int qte){
-        boolean ex = true;
-        try {
-            if (conn.isClosed()) conn = connectBD.connect();
+    private void CountProfit(){
+        String stQte = tfQte.getText().trim();
+        if (!stQte.equals("0")) {
 
             try {
-                String query = "SELECT sum(كمية_مخزنة - كمية_مستهلكة ) AS كمية FROM تخزين_منتج WHERE معرف_المنتج = ? AND كمية_مخزنة - كمية_مستهلكة > 0 ;";
-                PreparedStatement preparedStmt = conn.prepareStatement(query);
-                preparedStmt.setInt(1,this.selectedProduct.getId());
-                ResultSet resultSet = preparedStmt.executeQuery();
-                if (resultSet.next()){
-                    int qteProduct = resultSet.getInt("كمية");
-                    if (qteProduct < qte ) ex = false;
-                }else {
-                    ex = false;
-                }
-            }catch (Exception e){
+                String stPriceNew = tfPriceUnit.getText().trim();
+                double priceNew = Double.parseDouble(stPriceNew);
+
+
+                tfPrice.setText(String.format(Locale.FRANCE, "%,.2f", priceNew * qte));
+                tfNetProfit.setText(String.format(Locale.FRANCE, "%,.2f", ((priceNew * qte) - cost)));
+
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            conn.close();
-        }catch (Exception e){
-            e.printStackTrace();
         }
-        return ex;
-    }
-
-    private double getPrice(int qte){
-        double price = 0;
-        try {
-            if (conn.isClosed()) conn = connectBD.connect();
-            String query = "SELECT كمية_مخزنة - كمية_مستهلكة AS الكمية, سعر_البيع FROM تخزين_منتج WHERE" +
-                    " معرف_المنتج = ? AND  كمية_مخزنة - كمية_مستهلكة > 0 ORDER BY (تاريخ_التخزين) ASC;";
-            PreparedStatement preparedStmt = conn.prepareStatement(query);
-            preparedStmt.setInt(1,this.selectedProduct.getId());
-            ResultSet resultSet = preparedStmt.executeQuery();
-
-            while (resultSet.next()){
-                int qtePro = resultSet.getInt("الكمية");
-                int pricePro = resultSet.getInt("سعر_البيع");
-
-                if (qtePro >= qte){
-                    price += (pricePro * qte);
-                    break;
-                }else {
-                    price += (pricePro * qtePro);
-                    qte -= qtePro;
-                }
-            }
-            conn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return price;
-    }
-
-    private double getCost(int qte){
-        double cost = 0;
-
-        return cost;
     }
 
     @FXML
